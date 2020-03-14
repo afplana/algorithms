a4a769d15c06105ac922bbe51ce504431731b9b1
aplanas9009 <aplanas9009@gmail.com>
2020-03-14 14:24:33 +0200

    Add implementation of QuickUnion for Union Find

---
+++ src/test/java/me/coursera/algorithms/unionfind/QuickUnionUFTest.java
@@ -0,0 +1,37 @@
+package me.coursera.algorithms.unionfind;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QuickUnionUFTest {
+
+    QuickUnionUF quickUnionUF;
+
+    @BeforeEach
+    void setUp() {
+        quickUnionUF = new QuickUnionUF(10);
+        quickUnionUF.union(0, 5);
+    }
+
+    @Test
+    void root() {
+        assertEquals(5, quickUnionUF.root(0));
+        assertTrue(quickUnionUF.connected(0, 5));
+    }
+
+    @Test
+    void connected() {
+        assertTrue(quickUnionUF.connected(5, 0));
+        assertFalse(quickUnionUF.connected(8, 0));
+    }
+
+    @Test
+    void union() {
+        quickUnionUF.union(2, 5);
+        assertTrue(quickUnionUF.connected(5, 2));
+        assertEquals(5, quickUnionUF.root(2));
+        assertNotEquals(2, quickUnionUF.root(5));
+    }
+}
